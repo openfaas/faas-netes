6d893869a79fd82affd1488d61f664b15ceb4971
Alex Ellis <alexellis2@gmail.com>
2017-09-06 09:29:24 +0100

    Move to provider package
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ vendor/github.com/alexellis/faas-provider/types/requests.go
@@ -0,0 +1,9 @@
+// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package types
+
+type ScaleServiceRequest struct {
+	ServiceName string `json:"serviceName"`
+	Replicas    uint64 `json:"replicas"`
+}
