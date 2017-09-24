2c29d1a8784e997a969bc9c064e0c13676b5a07d
Elliott Beach <elliott2.71828@gmail.com>
2017-09-24 18:02:55 -0500

    enabled http timeout configuration Signed-off-by: Elliott Beach <elliott2.71828@gmail.com>

---
+++ types/read_config.go
@@ -0,0 +1,56 @@
+// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+package types
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+// OsEnv implements interface to wrap os.Getenv
+type OsEnv struct {
+}
+
+// Getenv wraps os.Getenv
+func (OsEnv) Getenv(key string) string {
+	return os.Getenv(key)
+}
+
+// HasEnv provides interface for os.Getenv
+type HasEnv interface {
+	Getenv(key string) string
+}
+
+// ReadConfig constitutes config from env variables
+type ReadConfig struct {
+}
+
+func parseIntValue(val string, fallback int) int {
+	if len(val) > 0 {
+		parsedVal, parseErr := strconv.Atoi(val)
+		if parseErr == nil && parsedVal >= 0 {
+			return parsedVal
+		}
+	}
+	return fallback
+}
+
+// Read fetches config from environmental variables.
+func (ReadConfig) Read(hasEnv HasEnv) BootstrapConfig {
+	cfg := BootstrapConfig{}
+
+	readTimeout := parseIntValue(hasEnv.Getenv("read_timeout"), 8)
+	writeTimeout := parseIntValue(hasEnv.Getenv("write_timeout"), 8)
+
+	cfg.ReadTimeout = time.Duration(readTimeout) * time.Second
+	cfg.WriteTimeout = time.Duration(writeTimeout) * time.Second
+
+	return cfg
+}
+
+// BootstrapConfig for the process.
+type BootstrapConfig struct {
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
