cf4962d092e3007856b2813775c1254de4047504
Alex Ellis <alexellis2@gmail.com>
2017-12-21 09:33:03 +0000

    Update to FaaSProvider 0.5 with health endpoint
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- handlers/health.go
+++ handlers/health.go
@@ -2,7 +2,7 @@ package handlers
 
 import "net/http"
 
-// MakeHealthHandler returns 200/OK when healhty
+// MakeHealthHandler returns 200/OK when healthy
 func MakeHealthHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
