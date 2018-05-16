ed03b942c46e954542b327598a9301045980a664
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-05-16 11:01:28 +0100

    Fix 198 to optimize fetching single functions
    An optimization is introduced to the single replica reader which
    means we use "Get" rather than "List" and filtering in memory.
    I originally wrote the List/filter approach with the intention of
    applying this change later. @imikushin from VMware suggested this
    change and raised PR196 - since there were some issues with
    process, Kubernetes version used and (lack of) testing I've
    implemented the change and done testing for this.
    
    [x] List functions gives correct content-type and list
    [x] Individual function queries return the correct data and
    content-type
    [x] Individual function queries for non-existant functions give
    404 when not found
    
    Also tested through the OpenFaaS UI including testing that
    there was no regression for the readiness feature.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- handlers/reader.go
+++ handlers/reader.go
@@ -5,47 +5,16 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/openfaas/faas/gateway/requests"
+	v1beta1 "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
-
-func getServiceList(functionNamespace string, clientset *kubernetes.Clientset) ([]requests.Function, error) {
-	functions := []requests.Function{}
-
-	listOpts := metav1.ListOptions{
-		LabelSelector: "faas_function",
-	}
-
-	res, err := clientset.ExtensionsV1beta1().Deployments(functionNamespace).List(listOpts)
-
-	if err != nil {
-		return nil, err
-	}
-
-	for _, item := range res.Items {
-		var replicas uint64
-		if item.Spec.Replicas != nil {
-			replicas = uint64(*item.Spec.Replicas)
-		}
-
-		labels := item.Spec.Template.Labels
-		function := requests.Function{
-			Name:              item.Name,
-			Replicas:          replicas,
-			Image:             item.Spec.Template.Spec.Containers[0].Image,
-			AvailableReplicas: uint64(item.Status.AvailableReplicas),
-			InvocationCount:   0,
-			Labels:            &labels,
-		}
-
-		functions = append(functions, function)
-	}
-	return functions, nil
-}
 
 // MakeFunctionReader handler for reading functions deployed in the cluster as deployments.
 func MakeFunctionReader(functionNamespace string, clientset *kubernetes.Clientset) http.HandlerFunc {
@@ -64,4 +33,71 @@ func MakeFunctionReader(functionNamespace string, clientset *kubernetes.Clientse
 		w.WriteHeader(http.StatusOK)
 		w.Write(functionBytes)
 	}
+}
+
+func getServiceList(functionNamespace string, clientset *kubernetes.Clientset) ([]requests.Function, error) {
+	functions := []requests.Function{}
+
+	listOpts := metav1.ListOptions{
+		LabelSelector: "faas_function",
+	}
+
+	res, err := clientset.ExtensionsV1beta1().Deployments(functionNamespace).List(listOpts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range res.Items {
+		function := readFunction(item)
+		if function != nil {
+			functions = append(functions, *function)
+		}
+	}
+	return functions, nil
+}
+
+// getService returns a function/service or nil if not found
+func getService(functionNamespace string, functionName string, clientset *kubernetes.Clientset) (*requests.Function, error) {
+
+	getOpts := metav1.GetOptions{}
+
+	item, err := clientset.ExtensionsV1beta1().Deployments(functionNamespace).Get(functionName, getOpts)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	if item != nil {
+
+		function := readFunction(*item)
+		if function != nil {
+			return function, nil
+		}
+	}
+
+	return nil, fmt.Errorf("function: %s not found", functionName)
+}
+
+func readFunction(item v1beta1.Deployment) *requests.Function {
+	var replicas uint64
+	if item.Spec.Replicas != nil {
+		replicas = uint64(*item.Spec.Replicas)
+	}
+
+	labels := item.Spec.Template.Labels
+	function := requests.Function{
+		Name:              item.Name,
+		Replicas:          replicas,
+		Image:             item.Spec.Template.Spec.Containers[0].Image,
+		AvailableReplicas: uint64(item.Status.AvailableReplicas),
+		InvocationCount:   0,
+		Labels:            &labels,
+	}
+
+	return &function
 }
