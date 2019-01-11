03a0a3c6586fa93d12d76f601fbab9f3d91ac71e
Alex Ellis (VMware) <alexellis2@gmail.com>
2019-01-11 09:42:17 +0000

    Update test for empty list of secrets
    - test moved from checking for 0 bytes to the text null
    specifically.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- handlers/secrets_api_test.go
+++ handlers/secrets_api_test.go
@@ -31,7 +31,7 @@ func Test_SecretsHandler(t *testing.T) {
 
 		resp := w.Result()
 		if resp.StatusCode != http.StatusAccepted {
-			t.Errorf("expected status code '%d', got '%d'", http.StatusAccepted, resp.StatusCode)
+			t.Errorf("want status code '%d', got '%d'", http.StatusAccepted, resp.StatusCode)
 		}
 
 		actualSecret, err := kube.CoreV1().Secrets(namespace).Get("testsecret", metav1.GetOptions{})
@@ -40,17 +40,17 @@ func Test_SecretsHandler(t *testing.T) {
 		}
 
 		if actualSecret.Name != "testsecret" {
-			t.Errorf("expected secret with name: 'testsecret', got: '%s'", actualSecret.Name)
+			t.Errorf("want secret with name: 'testsecret', got: '%s'", actualSecret.Name)
 		}
 
 		managedBy := actualSecret.Labels[secretLabel]
 		if managedBy != secretLabelValue {
-			t.Errorf("expected secret to be managed by '%s', got: '%s'", secretLabelValue, managedBy)
+			t.Errorf("want secret to be managed by '%s', got: '%s'", secretLabelValue, managedBy)
 		}
 
 		actualValue := actualSecret.StringData[secretName]
 		if actualValue != secretValue {
-			t.Errorf("expected secret value: '%s', got: '%s'", secretValue, actualValue)
+			t.Errorf("want secret value: '%s', got: '%s'", secretValue, actualValue)
 		}
 	})
 
@@ -64,7 +64,7 @@ func Test_SecretsHandler(t *testing.T) {
 
 		resp := w.Result()
 		if resp.StatusCode != http.StatusAccepted {
-			t.Errorf("expected status code '%d', got '%d'", http.StatusAccepted, resp.StatusCode)
+			t.Errorf("want status code '%d', got '%d'", http.StatusAccepted, resp.StatusCode)
 		}
 
 		actualSecret, err := kube.CoreV1().Secrets(namespace).Get("testsecret", metav1.GetOptions{})
@@ -73,17 +73,17 @@ func Test_SecretsHandler(t *testing.T) {
 		}
 
 		if actualSecret.Name != "testsecret" {
-			t.Errorf("expected secret with name: 'testsecret', got: '%s'", actualSecret.Name)
+			t.Errorf("want secret with name: 'testsecret', got: '%s'", actualSecret.Name)
 		}
 
 		managedBy := actualSecret.Labels[secretLabel]
 		if managedBy != secretLabelValue {
-			t.Errorf("expected secret to be managed by '%s', got: '%s'", secretLabelValue, managedBy)
+			t.Errorf("want secret to be managed by '%s', got: '%s'", secretLabelValue, managedBy)
 		}
 
 		actualValue := actualSecret.StringData[secretName]
 		if actualValue != newSecretValue {
-			t.Errorf("expected secret value: '%s', got: '%s'", newSecretValue, actualValue)
+			t.Errorf("want secret value: '%s', got: '%s'", newSecretValue, actualValue)
 		}
 	})
 
@@ -95,7 +95,7 @@ func Test_SecretsHandler(t *testing.T) {
 
 		resp := w.Result()
 		if resp.StatusCode != http.StatusOK {
-			t.Errorf("expected status code '%d', got '%d'", http.StatusOK, resp.StatusCode)
+			t.Errorf("want status code '%d', got '%d'", http.StatusOK, resp.StatusCode)
 		}
 
 		decoder := json.NewDecoder(resp.Body)
@@ -106,13 +106,14 @@ func Test_SecretsHandler(t *testing.T) {
 			t.Error(err)
 		}
 
-		if len(secretList) != 1 {
-			t.Errorf("expected 1 secret, got %d", len(secretList))
+		secretWant := 1
+		if len(secretList) != secretWant {
+			t.Errorf("want %d secret, got %d", secretWant, len(secretList))
 		}
 
 		actualSecret := secretList[0]
 		if actualSecret.Name != secretName {
-			t.Errorf("expected secret name: '%s', got: '%s'", secretName, actualSecret.Name)
+			t.Errorf("want secret name: '%s', got: '%s'", secretName, actualSecret.Name)
 		}
 	})
 
@@ -126,12 +127,12 @@ func Test_SecretsHandler(t *testing.T) {
 
 		resp := w.Result()
 		if resp.StatusCode != http.StatusAccepted {
-			t.Errorf("expected status code '%d', got '%d'", http.StatusAccepted, resp.StatusCode)
+			t.Errorf("want status code '%d', got '%d'", http.StatusAccepted, resp.StatusCode)
 		}
 
 		actualSecret, err := kube.CoreV1().Secrets(namespace).Get("testsecret", metav1.GetOptions{})
 		if err == nil {
-			t.Errorf("expected not found error, got secret payload '%s'", actualSecret)
+			t.Errorf("want not found error, got secret payload '%s'", actualSecret)
 		}
 	})
 }
@@ -148,11 +149,11 @@ func Test_SecretsHandler_ListEmpty(t *testing.T) {
 
 	resp := w.Result()
 	if resp.StatusCode != http.StatusOK {
-		t.Errorf("expected status code '%d', got '%d'", http.StatusOK, resp.StatusCode)
+		t.Errorf("want status code '%d', got '%d'", http.StatusOK, resp.StatusCode)
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
 
-	if len(body) == 0 {
-		t.Errorf(`want empty list to be valid json "[]", but was empty string.`)
+	if string(body) == "null" {
+		t.Errorf(`want empty list to be valid json i.e. "[]", but was %q`, string(body))
 	}
 }
