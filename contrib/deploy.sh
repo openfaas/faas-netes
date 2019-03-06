b6b80c0c6f69d67f9ab2049bfafc7b1de6c08b08
Rishabh Gupta <r.g.gupta@outlook.com>
2019-03-06 15:05:34 +0530

    Install openfaas using helm template
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 export KUBECONFIG="$(kind get kubeconfig-path)"
+
+echo "Applying namespaces"
+kubectl apply -f ./namespaces.yml
+
+PASSWORD="something_random"
+kubectl -n openfaas create secret generic basic-auth \
+--from-literal=basic-auth-user=admin \
+--from-literal=basic-auth-password="$PASSWORD"
+
+helm template ./chart/openfaas \
+    --name openfaas \
+    --namespace openfaas  \
+    --set basic_auth=true \
+    --set functionNamespace=openfaas-fn > ./openfaas.yaml
+
+kubectl apply -f ./openfaas.yaml
