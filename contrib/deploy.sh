3899fb9220224999d3437bcf9ef1a5553738931b
Rishabh Gupta <r.g.gupta@outlook.com>
2019-04-03 15:23:12 +0530

    Fix password location
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -5,7 +5,9 @@ export KUBECONFIG="$(kind get kubeconfig-path)"
 echo "Applying namespaces"
 kubectl apply -f ./namespaces.yml
 
+echo $(head -c 16 /dev/urandom| $sha_cmd | cut -d " " -f 1) > ./password.txt # Store password in password.txt file
 PASSWORD=$(cat ./password.txt)
+
 kubectl -n openfaas create secret generic basic-auth \
 --from-literal=basic-auth-user=admin \
 --from-literal=basic-auth-password="$PASSWORD"
