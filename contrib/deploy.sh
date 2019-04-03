d56935c5863aeb86b5324e219ec238a99cbcf449
Rishabh Gupta <r.g.gupta@outlook.com>
2019-04-03 15:35:42 +0530

    Fixed shasum command
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -5,7 +5,7 @@ export KUBECONFIG="$(kind get kubeconfig-path)"
 echo "Applying namespaces"
 kubectl apply -f ./namespaces.yml
 
-echo $(head -c 16 /dev/urandom| $sha_cmd | cut -d " " -f 1) > ./password.txt # Store password in password.txt file
+echo $(head -c 16 /dev/urandom| sha256sum | cut -d " " -f 1) > ./password.txt # Store password in password.txt file
 PASSWORD=$(cat ./password.txt)
 
 kubectl -n openfaas create secret generic basic-auth \
