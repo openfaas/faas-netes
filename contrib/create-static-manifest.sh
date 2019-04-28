4b0549b76a1ae984456837c68d124e4a1e800965
Lucas Roesler <roesler.lucas@gmail.com>
2019-04-28 14:18:36 +0200

    Set function namespace during yaml generation
    **What**
    - Set the function namespace when using `helm template` in the
    `create-static-manifest` script.  Allow overriding this value as the 5th
    argument, using `openfaas-fn` as the default.
    -  This is needed to preserve the original configuration from prior to
    the introduction of the `create-static-manifest` in #424
    
    Resolves #430
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- contrib/create-static-manifest.sh
+++ contrib/create-static-manifest.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 
-# Usage: ./create-static-manifest.sh ./chart/openfaas ./yaml
+# Uses `helm template` to generate static yaml manifests for installation via `kubectl apply -f`.
+#
+# You can safely override the chart, the output directory, the values file used, the installation
+# namespace. and the function namespace.
+#
+# This script is used to generate the default yaml installations included in the faas-netes repo.
+#
+# Usage:
+#   $ ./create-static-manifest.sh
+#   $ ./create-static-manifest.sh ./chart/openfaas ./yaml
+#   $ ./create-static-manifest.sh ./chart/openfaas ./yaml value.yaml
+#   $ ./create-static-manifest.sh ./chart/openfaas ./yaml value.yaml openfaas
+#   $ ./create-static-manifest.sh ./chart/openfaas ./yaml value.yaml openfaas openfaas-fn
 
 CHART_DIR=${1:-"./chart/openfaas"}
 OUTPUT_DIR=${2:-"./yaml"}
 VALUESNAME=${3:-"values.yaml"}
 NAMEPSPACE=${4:-"openfaas"}
+FUNCTIONNAMESPACE=${5:-"openfaas-fn"}
 
 TEMPLATE_FILE=$(find $CHART_DIR/templates/*.yaml -type f)
 for filepath in $TEMPLATE_FILE; do
     filename=$(basename $filepath)
     outputname="${filename%%.*}.yml"
-    helm template "$CHART_DIR" --name=faas --namespace="$NAMEPSPACE" -x templates/$filename --values="$CHART_DIR/$VALUESNAME" \
+    # Use helm to generate the yaml and then use sed to remove the helm specific lables/annotations.
+    helm template "$CHART_DIR" --name=faas --namespace="$NAMEPSPACE" --set "functionNamespace=$FUNCTIONNAMESPACE" -x templates/$filename --values="$CHART_DIR/$VALUESNAME" \
         | sed -E '/(chart:)|(release:)|(heritage:)/d' \
         | sed -E '/# Source:/d' \
         | sed -E '/^$/d' \
         > $OUTPUT_DIR/$outputname \
         && echo "wrote $OUTPUT_DIR/$outputname"
 
+    # Remove any  files that are exactly 4 characters, the are "empty" files, we expect that these
+    # files will only contain "---\n"
     find $OUTPUT_DIR -size 4c -delete
 done
