85f5609ab9d6a7dc26a02f84772f0d4e6ff2cfad
stefanprodan <stefan.prodan@gmail.com>
2019-06-07 18:52:32 +0300

    Add k8s package to be used for Kubernetes operations Extract liveness and readiness probes logic from handlers into k8s package Implement HTTP probe custom path and initial delay duration by overriding the defaults with annotations
    Signed-off-by: stefanprodan <stefan.prodan@gmail.com>

---
+++ k8s/probes_test.go
@@ -0,0 +1,126 @@
+package k8s
+
+import (
+	"github.com/openfaas/faas/gateway/requests"
+	"testing"
+)
+
+func Test_makeProbes_useExec(t *testing.T) {
+	f := mockFactory()
+
+	request := requests.CreateFunctionRequest{
+		Service:                "testfunc",
+		ReadOnlyRootFilesystem: false,
+	}
+
+	probes, err := f.MakeProbes(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if probes.Readiness.Exec == nil {
+		t.Errorf("Readiness probe should have had exec handler")
+		t.Fail()
+	}
+	if probes.Liveness.Exec == nil {
+		t.Errorf("Liveness probe should have had exec handler")
+		t.Fail()
+	}
+}
+
+func Test_makeProbes_useHTTPProbe(t *testing.T) {
+	f := mockFactory()
+	f.Config.HTTPProbe = true
+
+	request := requests.CreateFunctionRequest{
+		Service:                "testfunc",
+		ReadOnlyRootFilesystem: false,
+	}
+
+	probes, err := f.MakeProbes(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if probes.Readiness.HTTPGet == nil {
+		t.Errorf("Readiness probe should have had HTTPGet handler")
+		t.Fail()
+	}
+	if probes.Liveness.HTTPGet == nil {
+		t.Errorf("Liveness probe should have had HTTPGet handler")
+		t.Fail()
+	}
+}
+
+func Test_makeProbes_useCustomHTTPProbe(t *testing.T) {
+	f := mockFactory()
+	f.Config.HTTPProbe = true
+	customPath := "/healthz"
+	request := requests.CreateFunctionRequest{
+		Service:                "testfunc",
+		ReadOnlyRootFilesystem: false,
+		Annotations: &map[string]string{
+			ProbePath: customPath,
+		},
+	}
+
+	probes, err := f.MakeProbes(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if probes.Readiness.HTTPGet == nil {
+		t.Errorf("Readiness probe should have had HTTPGet handler")
+		t.Fail()
+	}
+	if probes.Readiness.HTTPGet.Path != customPath {
+		t.Errorf("Readiness probe should have had HTTPGet handler set to %s", customPath)
+		t.Fail()
+	}
+
+	if probes.Liveness.HTTPGet == nil {
+		t.Errorf("Liveness probe should have had HTTPGet handler")
+		t.Fail()
+	}
+	if probes.Liveness.HTTPGet.Path != customPath {
+		t.Errorf("Readiness probe should have had HTTPGet handler set to %s", customPath)
+		t.Fail()
+	}
+}
+
+func Test_makeProbes_useCustomDurationHTTPProbe(t *testing.T) {
+	f := mockFactory()
+	f.Config.HTTPProbe = true
+	customDelay := "2m"
+
+	request := requests.CreateFunctionRequest{
+		Service:                "testfunc",
+		ReadOnlyRootFilesystem: false,
+		Annotations: &map[string]string{
+			ProbeInitialDelay: customDelay,
+		},
+	}
+
+	probes, err := f.MakeProbes(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if probes.Readiness.HTTPGet == nil {
+		t.Errorf("Readiness probe should have had HTTPGet handler")
+		t.Fail()
+	}
+	if probes.Readiness.InitialDelaySeconds != 120 {
+		t.Errorf("Readiness probe should have initial delay seconds set to %s", customDelay)
+		t.Fail()
+	}
+
+	if probes.Liveness.HTTPGet == nil {
+		t.Errorf("Liveness probe should have had HTTPGet handler")
+		t.Fail()
+	}
+	if probes.Liveness.InitialDelaySeconds != 120 {
+		t.Errorf("Readiness probe should have had HTTPGet handler set to %s", customDelay)
+		t.Fail()
+	}
+}
