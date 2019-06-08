e4219943a55c2a02188062c6cc87353e2bd76800
stefanprodan <stefan.prodan@gmail.com>
2019-06-08 11:04:20 +0300

    Add copyright header to k8s package
    Signed-off-by: stefanprodan <stefan.prodan@gmail.com>

--- k8s/factory_test.go
+++ k8s/factory_test.go
@@ -1,3 +1,6 @@
+// Copyright 2019 OpenFaaS Authors
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package k8s
 
 import "k8s.io/client-go/kubernetes/fake"
