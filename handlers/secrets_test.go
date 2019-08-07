2f60aed2f372682b025f45b4d96cf27f027c0835
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-08-07 09:04:53 +0100

    Sort env-vars by string name
    This issue fixes the non-determinism observed by @LucasRoesler
    and @bmcstdio which caused Function Pods to be re-created due
    to the order of environment variables changing in the Deployment
    and PodSpec.
    
    Ref: https://github.com/openfaas/faas-netes/issues/484
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- handlers/secrets_test.go
+++ handlers/secrets_test.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/openfaas/faas/gateway/requests"
+	types "github.com/openfaas/faas-provider/types"
 	appsv1 "k8s.io/api/apps/v1beta2"
 	apiv1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
