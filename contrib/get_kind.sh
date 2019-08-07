1ff40cb3fc413d78363e00e3b6819d2a3e15646c
Lucas Roesler <roesler.lucas@gmail.com>
2019-08-07 13:35:50 +0200

    Bump kind version to 0.4.0
    **What**
    - Set the kind version to 0.4.0 to match their install instructions
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- contrib/get_kind.sh
+++ contrib/get_kind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "go get -u sigs.k8s.io/kind"
+echo "go get -u sigs.k8s.io/kind@v0.4.0"
 
 export GO111MODULE="on"
-go get -u sigs.k8s.io/kind@0a6ceae
+go get -u sigs.k8s.io/kind@v0.4.0
