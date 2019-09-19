713323ac3117cf88834d6122ad912f2db1663bbd
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-09-19 17:19:31 +0100

    Add namespace to istio destination rules host
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo Installing with helm 👑
+
+helm repo add openfaas https://openfaas.github.io/faas-netes/
+
+kubectl apply -f \
+   https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
+
+# generate a random password
+PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
+
+kubectl -n openfaas create secret generic basic-auth \
+--from-literal=basic-auth-user=admin \
+--from-literal=basic-auth-password="$PASSWORD"
+
+helm upgrade openfaas --install openfaas/openfaas \
+    --namespace openfaas  \
+    --set basic_auth=true \
+    --set functionNamespace=openfaas-fn \
+    --set serviceType=LoadBalancer
+
