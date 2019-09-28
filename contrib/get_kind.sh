97cec3aab1d04a4b49e0c71d96264e12ae636041
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-09-28 19:01:59 +0100

    Move to KinD binary instead of fetching source
    Getting the KinD via binary is *much* faster,  Go modules now
    adds several minutes to the build / test time.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/get_kind.sh
+++ contrib/get_kind.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "go get -u sigs.k8s.io/kind@v0.4.0"
+# echo "go get -u sigs.k8s.io/kind@v0.4.0"
 
-export GO111MODULE="on"
-go get -u sigs.k8s.io/kind@v0.4.0
+# export GO111MODULE="on"
+# go get -u sigs.k8s.io/kind@v0.4.0
+
+# Edited by Alex Ellis - KinD via binary is *much* faster, 
+# Go modules now adds several minutes to the build / test time :-(
+curl -SLfs https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-linux-amd64 > kind-linux-amd64
+chmod +x kind-linux-amd64 
+sudo mv kind-linux-amd64 /usr/local/bin/kind
