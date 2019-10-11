38fb07be2329f7ebcc3eb2096361b339846b99a6
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-10-11 11:22:31 +0100

    Initial
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- handlers/proxy.go
+++ handlers/proxy.go
@@ -5,27 +5,83 @@ package handlers
 
 import (
 	"fmt"
+	"math/rand"
 	"net/url"
 	"strings"
+	"sync"
+
+	corelister "k8s.io/client-go/listers/core/v1"
 )
 
 // watchdogPort for the OpenFaaS function watchdog
 const watchdogPort = 8080
 
+func NewFunctionLookup(ns string, lister corelister.EndpointsLister) *FunctionLookup {
+	return &FunctionLookup{
+		DefaultNamespace: ns,
+		EndpointLister:   lister,
+		Listers:          map[string]corelister.EndpointsNamespaceLister{},
+		lock:             sync.RWMutex{},
+	}
+}
+
 type FunctionLookup struct {
 	DefaultNamespace string
+	EndpointLister   corelister.EndpointsLister
+	Listers          map[string]corelister.EndpointsNamespaceLister
+
+	lock sync.RWMutex
+}
+
+func (f *FunctionLookup) GetLister(ns string) corelister.EndpointsNamespaceLister {
+	f.lock.RLock()
+	defer f.lock.RUnlock()
+	return f.Listers[ns]
+}
+
+func (f *FunctionLookup) SetLister(ns string, lister corelister.EndpointsNamespaceLister) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	f.Listers[ns] = lister
+}
+
+func getNamespace(name, defaultNamespace string) string {
+	namespace := defaultNamespace
+	if strings.Contains(name, ".") {
+		namespace = name[:strings.LastIndexAny(name, ".")+1]
+	}
+	return namespace
 }
 
 func (l *FunctionLookup) Resolve(name string) (url.URL, error) {
-
-	urlStr := fmt.Sprintf("http://%s.%s:%d", name, l.DefaultNamespace, watchdogPort)
 	if strings.Contains(name, ".") {
 		err := l.verifyNamespace(name)
 		if err != nil {
 			return url.URL{}, err
 		}
-		urlStr = fmt.Sprintf("http://%s:%d", name, watchdogPort)
 	}
+
+	namespace := getNamespace(name, l.DefaultNamespace)
+
+	nsEndpointLister := l.GetLister(namespace)
+
+	if nsEndpointLister == nil {
+		l.SetLister(namespace, l.EndpointLister.Endpoints(namespace))
+
+		nsEndpointLister = l.GetLister(namespace)
+	}
+
+	svc, err := nsEndpointLister.Get(name)
+	if err != nil {
+		return url.URL{}, fmt.Errorf("error listing %s.%s %s", name, namespace, err.Error())
+	}
+
+	all := len(svc.Subsets[0].Addresses)
+	target := rand.Intn(all)
+
+	serviceIP := svc.Subsets[0].Addresses[target].IP
+
+	urlStr := fmt.Sprintf("http://%s.%s:%d", serviceIP, namespace, watchdogPort)
 
 	urlRes, err := url.Parse(urlStr)
 	if err != nil {
@@ -36,6 +92,9 @@ func (l *FunctionLookup) Resolve(name string) (url.URL, error) {
 }
 
 func (l *FunctionLookup) verifyNamespace(name string) error {
+	if name != "kube-system" {
+		return nil
+	}
 	// ToDo use global namepace parse and validation
-	return nil
+	return fmt.Errorf("namespace not allowed")
 }
