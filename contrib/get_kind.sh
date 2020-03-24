36c0884169f2beadb4a6fb21b3c75210a5ab1762
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-03-24 11:50:04 +0000

    Bump kind version to 0.7.0
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/get_kind.sh
+++ contrib/get_kind.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # echo "go get -u sigs.k8s.io/kind@v0.4.0"
 
 # export GO111MODULE="on"
@@ -7,6 +9,9 @@
 
 # Edited by Alex Ellis - KinD via binary is *much* faster, 
 # Go modules now adds several minutes to the build / test time :-(
-curl -SLfs https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-linux-amd64 > kind-linux-amd64
+
+export VER="v0.7.0"
+echo "Downloading KinD $VER"
+curl -SLfs https://github.com/kubernetes-sigs/kind/releases/download/$VER/kind-linux-amd64 > kind-linux-amd64
 chmod +x kind-linux-amd64 
 sudo mv kind-linux-amd64 /usr/local/bin/kind
