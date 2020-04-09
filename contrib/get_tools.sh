5f643e8bf6ea0ab6e89617fc1dcf60364c9adb84
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-04-09 18:28:05 +0100

    Fix kubectl into a specific version
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/get_tools.sh
+++ contrib/get_tools.sh
@@ -4,19 +4,20 @@ set -e
 
 KIND_VERSION="v0.7.0"
 HELM_VERSION=v3.0.3
-KUBE_VERSION=v0.7.0
+KUBE_VERSION=v1.17.0
 
 if [[ "$1" ]]; then
   KUBE_VERSION=$1
 fi
 
 echo ">>> Installing kind $KIND_VERSION"
-curl -sSLfo kind "https://github.com/kubernetes-sigs/kind/releases/download/$KIND_VERSION/kind-linux-amd64"
-chmod +x kind
+curl -sSLfo kind "https://github.com/kubernetes-sigs/kind/releases/download/$KIND_VERSION/kind-linux-amd64" && \
+chmod +x kind && \
 sudo mv kind /usr/local/bin/kind
 
 echo ">>> Installing kubectl"
-curl -sSLfo https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
+OS=linux
+curl -sSLfo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/$OS/amd64/kubectl && \
 chmod +x kubectl && \
 sudo mv kubectl /usr/local/bin/
 
