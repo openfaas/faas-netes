1e5ef7d981ff2e0ffee9325efcc41babbf656e13
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-04-09 18:33:56 +0100

    Update kind to its generated context
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/restart_port_forward.sh
+++ contrib/restart_port_forward.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEVENV=${OF_DEV_ENV:-kind-kind}
+DEVENV=${OF_DEV_ENV:-kind}
 
 if [ -f "of_${DEVENV}_portforward.pid" ]; then
     kill $(<of_${DEVENV}_portforward.pid)
@@ -8,5 +8,5 @@ fi
 
 # quietly start portforward and put it in the background, it will not
 # print every connection handled
-kubectl --context "$DEVENV" port-forward deploy/gateway -n openfaas 31112:8080 &>/dev/null & \
+kubectl --context "kind-$DEVENV" port-forward deploy/gateway -n openfaas 31112:8080 &>/dev/null & \
     echo -n "$!" > "of_${DEVENV}_portforward.pid"
--- contrib/stop_dev.sh
+++ contrib/stop_dev.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-DEVENV=${OF_DEV_ENV:-kind-kind}
+DEVENV=${OF_DEV_ENV:-kind}
 
 if [ -f "of_${DEVENV}_portforward.pid" ]; then
     kill $(<of_${DEVENV}_portforward.pid)
     rm "of_${DEVENV:-kind}_portforward.pid"
 fi
 
-kind delete cluster --name="$DEVENV"
+kind delete cluster --name "$DEVENV"
