533d0249722e98ab8f246da4f0c5e960bb39d116
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-04-23 12:49:55 +0100

    Enable verbose KinD
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/create_cluster.sh
+++ contrib/create_cluster.sh
@@ -5,7 +5,7 @@ KUBE_VERSION=v1.17.0
 
 echo ">>> Creating Kubernetes ${KUBE_VERSION} cluster ${DEVENV}"
 
-kind create cluster --wait 5m --image kindest/node:${KUBE_VERSION} --name "$DEVENV"
+kind create cluster --wait 5m --image kindest/node:${KUBE_VERSION} --name "$DEVENV" -v 1
 
 echo ">>> Waiting for CoreDNS"
 kubectl --context "kind-$DEVENV" -n kube-system rollout status deployment/coredns
