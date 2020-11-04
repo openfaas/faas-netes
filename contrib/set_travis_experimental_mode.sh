00a26e5bdb8766df940892b255cbf14e6fa48fe3
Akos Veres <veres@akos.me>
2020-11-04 18:20:27 +0000

    Move CI steps from travis to github actions
    Signed-off-by: Akos Veres <veres@akos.me>

--- contrib/set_travis_experimental_mode.sh
+++
@@ -1,3 +0,0 @@
-# Enable pre-release docker features in the travis build.
-# Can't be in .travis.yml as travis has trouble parsing the braces.
-echo "{\"experimental\": true}" | sudo tee /etc/docker/daemon.json
