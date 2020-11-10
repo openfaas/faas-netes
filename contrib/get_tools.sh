fc4b4553d8d8c64b518bd3be5e9f6f11994149c0
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-11-10 11:08:24 +0000

    Update to latest K8s versions
    Uses arkade to download CLIs for brevity
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/get_tools.sh
+++ contrib/get_tools.sh
@@ -2,30 +2,24 @@
 
 set -e
 
-KIND_VERSION="v0.7.0"
-HELM_VERSION=v3.0.3
-KUBE_VERSION=v1.17.0
+KIND_VERSION="v0.9.0"
+HELM_VERSION="v3.4.0"
+KUBE_VERSION="v1.18.8"
+ARKADE_VERSION="0.6.21"
+
+echo "Downloading arkade"
+
+curl -SLs https://github.com/alexellis/arkade/releases/download/$ARKADE_VERSION/arkade > arkade
+chmod +x ./arkade
+
 
 if [[ "$1" ]]; then
   KUBE_VERSION=$1
 fi
 
-echo ">>> Installing kind $KIND_VERSION"
-curl -sSLfo kind "https://github.com/kubernetes-sigs/kind/releases/download/$KIND_VERSION/kind-linux-amd64" && \
-chmod +x kind && \
-sudo mv kind /usr/local/bin/kind
+./arkade get kind --version $KIND_VERSION
+./arkade get helm --version $HELM_VERSION
+./arkade get kubectl --version $KUBE_VERSION
+./arkade get faas-cli
 
-echo ">>> Installing kubectl"
-OS=linux
-curl -sSLfo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/$OS/amd64/kubectl && \
-chmod +x kubectl && \
-sudo mv kubectl /usr/local/bin/
-
-echo ">>> Installing Helm v3"
-curl -sSLf https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz | tar xz && \
-sudo mv linux-amd64/helm /usr/local/bin/ && \
-rm -rf linux-amd64
-
-echo ">>> Installing faas-cli"
-curl -sSLf https://cli.openfaas.com | sudo sh
-faas-cli version
+sudo mv $HOME/.arkade/bin/* /usr/local/bin/
