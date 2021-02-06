ea04fd7977d91c029d527a8fedd812e9a8c67999
Lucas Roesler <roesler.lucas@gmail.com>
2021-02-06 11:46:13 +0100

    Add arg to skip tests during builds
    **What**
    - Add a build arg that allows us to explicitly skip tests during docker
      builds. This can be used for quicker iterations locally and is always
      set during pull request CI multi-arch builds.
    
      Note that the publish CI flow _still_ runs tests during the native
      build (amd64) because the publish flow does not otherwise run the
      tests
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# skip tests if it is non-empty, can be any value
+if [[ ! -z "$SKIP_TESTS" ]]; then
+    echo "skipping tests by request"
+    exit 0
+fi
+
 if [[ ! -v BUILDPLATFORM ]]; then
     CGO_ENABLED=0 go test -v ./...
 elif [[ "$BUILDPLATFORM" == "$TARGETPLATFORM" ]]; then
