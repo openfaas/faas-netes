e0a7c5c50fe0c50c20b3a225217820f220fd2b62
Lucas Roesler <roesler.lucas@gmail.com>
2021-05-22 10:00:55 +0200

    fix: Improve the error behavior in make start-kind
    Add various checks for existing data and ask for user input before
    continuing during the cluster creation.
    
    Ignore certain errors, such as killing the port-forward, it is ok for
    this command to fail because it indicates that the background task is
    already stopped.
    
    Improve the formatting of the output to make it easier to read.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- contrib/create_cluster.sh
+++ contrib/create_cluster.sh
@@ -3,9 +3,25 @@
 DEVENV=${OF_DEV_ENV:-kind}
 KUBE_VERSION=v1.18.19
 
-echo ">>> Creating Kubernetes ${KUBE_VERSION} cluster ${DEVENV}"
+EXISTS=$(kind get clusters | grep "$DEVENV")
 
-kind create cluster --wait 5m --image kindest/node:${KUBE_VERSION} --name "$DEVENV" -v 1
+if [ "$EXISTS" = "$DEVENV" ]; then
+    while true; do
+    read -p "Kind cluster '$DEVENV' already exists, do you want to continue with this cluster? (yes/no) " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit 1;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+fi
+
+
+if [ "$EXISTS" != "$DEVENV" ]; then
+    echo ">>> Creating Kubernetes ${KUBE_VERSION} cluster ${DEVENV}"
+
+    kind create cluster --wait 5m --image kindest/node:${KUBE_VERSION} --name "$DEVENV" -v 1
+fi
 
 echo ">>> Waiting for CoreDNS"
 kubectl --context "kind-$DEVENV" -n kube-system rollout status deployment/coredns
--- contrib/run_function.sh
+++ contrib/run_function.sh
@@ -13,21 +13,30 @@ then
 fi
 
 if [ -f "of_${DEVENV}_portforward.pid" ]; then
-    kill $(<of_${DEVENV}_portforward.pid)
+    kill "$(<of_${DEVENV}_portforward.pid)" > /dev/null 2> /dev/null || :
 fi
 
 # quietly start portforward and put it in the background, it will not
 # print every connection handled
-kubectl --context "kind-$DEVENV" port-forward deploy/gateway -n openfaas 31112:8080 &>/dev/null & \
+kubectl --context "kind-$DEVENV" port-forward deploy/gateway -n openfaas 8080:8080 &>/dev/null & \
     echo -n "$!" > "of_${DEVENV}_portforward.pid"
 
 # port-forward needs some time to start
 sleep 10
 
-export OPENFAAS_URL=http://127.0.0.1:31112
+export OPENFAAS_URL=http://127.0.0.1:8080
 
 # Login into the gateway
+echo ""
+echo "Login to the gateway..."
 cat ./password.txt | faas-cli login --username admin --password-stdin
+
+IS_DEPLOYED=$(faas-cli list | grep echo | wc -c)
+if [ $IS_DEPLOYED -gt 0 ];then
+    echo ""
+    echo "test function 'echo' already deployed"
+    exit;
+fi
 
 # Deploy via the REST API which can test either the controller or operator
 faas-cli deploy --image=functions/alpine:latest --fprocess=cat --name "echo"
