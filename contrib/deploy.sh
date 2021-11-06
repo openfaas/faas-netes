bdeddfc80f21be5bd546dd0d1011a1fe504bdcf6
Lucas Roesler <roesler.lucas@gmail.com>
2021-11-06 13:39:02 +0100

    ci: use locally built image in the e2e tests
    Load the just built image into the kind cluster before running the e2e
    tests. This ensure we are testing the changes on the branch before we
    publish them.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -4,6 +4,7 @@ set -e
 
 DEVENV=${OF_DEV_ENV:-kind}
 OPERATOR=${OPERATOR:-0}
+IMAGE=${IMAGE:-""}
 
 echo ""
 echo "Applying namespaces"
@@ -20,15 +21,15 @@ fi
 
 PASSWORD_EXISTS=$(kubectl get secret -n openfaas basic-auth | wc -c)
 if [ $PASSWORD_EXISTS -eq 0 ]; then
-    PASSWORD=$(head -c 16 /dev/urandom| $sha_cmd | cut -d " " -f 1)
-    echo -n $PASSWORD > password.txt
+    PASSWORD=$(head -c 16 /dev/urandom | $sha_cmd | cut -d " " -f 1)
+    echo -n $PASSWORD >password.txt
 
     kubectl --context "kind-$DEVENV" -n openfaas create secret generic basic-auth \
-    --from-literal=basic-auth-user=admin \
-    --from-literal=basic-auth-password="$PASSWORD"
+        --from-literal=basic-auth-user=admin \
+        --from-literal=basic-auth-password="$PASSWORD"
 else
     PASSWORD=$(kubectl get secret -n openfaas basic-auth -o=go-template='{{index .data "basic-auth-password"}}' | base64 --decode)
-    echo -n $PASSWORD > password.txt
+    echo -n $PASSWORD >password.txt
 fi
 
 CREATE_OPERATOR=false
@@ -39,20 +40,35 @@ fi
 echo ""
 echo "Waiting for helm install to complete."
 
-helm upgrade \
-    --kube-context "kind-$DEVENV" \
-    --install \
-    openfaas \
-    ./chart/openfaas \
-    --namespace openfaas  \
-    --set basic_auth=true \
-    --set functionNamespace=openfaas-fn \
-    --set operator.create=$CREATE_OPERATOR
+if [ "z${IMAGE}" == "z" ]; then
+    helm upgrade \
+        --kube-context "kind-$DEVENV" \
+        --install \
+        openfaas \
+        ./chart/openfaas \
+        --namespace openfaas \
+        --set basic_auth=true \
+        --set functionNamespace=openfaas-fn \
+        --set operator.create=$CREATE_OPERATOR
+else
+    helm upgrade \
+        --kube-context "kind-$DEVENV" \
+        --install \
+        openfaas \
+        ./chart/openfaas \
+        --namespace openfaas \
+        --set basic_auth=true \
+        --set functionNamespace=openfaas-fn \
+        --set openfaasImagePullPolicy=IfNotPresent \
+        --set operator.create=$CREATE_OPERATOR \
+        --set operator.image=$IMAGE \
+        --set faasnetes.image=$IMAGE
+fi
 
 if [ "${OPERATOR}" == "1" ]; then
 
     kubectl --context "kind-$DEVENV" patch -n openfaas deploy/gateway \
-      -p='[{"op": "add", "path": "/spec/template/spec/containers/1/command", "value": ["./faas-netes", "-operator=true"]} ]' --type=json
+        -p='[{"op": "add", "path": "/spec/template/spec/containers/1/command", "value": ["./faas-netes", "-operator=true"]} ]' --type=json
 fi
 
 kubectl --context "kind-$DEVENV" rollout status deploy/prometheus -n openfaas
