ee589f3968f196da36e44156b1fbca20dee9b503
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2024-02-22 15:51:07 +0000

    Update CE build with latest versions
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- go.mod
+++ go.mod
@@ -39,6 +39,7 @@ require (
 	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
--- vendor/github.com/prometheus/client_golang/prometheus/process_collector_wasip1.go
+++ vendor/github.com/prometheus/client_golang/prometheus/process_collector_wasip1.go
@@ -1,10 +1,9 @@
-// Copyright 2015 go-swagger maintainers
-//
+// Copyright 2023 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,13 +11,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !go1.8
-// +build !go1.8
+//go:build wasip1
+// +build wasip1
 
-package swag
+package prometheus
 
-import "net/url"
+func canCollectProcess() bool {
+	return false
+}
 
-func pathUnescape(path string) (string, error) {
-	return url.QueryUnescape(path)
+func (*processCollector) processCollect(chan<- Metric) {
+	// noop on this platform
+	return
 }
