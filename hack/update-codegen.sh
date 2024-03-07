0b7a381d60815046525afab96ee93a0b988c925f
Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>
2024-03-07 15:49:54 +0100

    Update faas-netes for new codegen
    Signed-off-by: Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,41 +1,29 @@
 #!/usr/bin/env bash
 
-# copied from: https://github.com/weaveworks/flagger/tree/master/hack
+# modified from: https://github.com/kubernetes/sample-controller/blob/master/hack/update-codegen.sh
+# Apache-2.0, Copyright 2017 The Kubernetes Authors
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(git rev-parse --show-toplevel)
-
-# Grab code-generator version from go.sum.
-CODEGEN_VERSION=$(grep 'k8s.io/code-generator' go.sum | awk '{print $2}' | head -1)
-CODEGEN_PKG=$(echo `go env GOPATH`"/pkg/mod/k8s.io/code-generator@${CODEGEN_VERSION}")
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 echo ">> Using ${CODEGEN_PKG}"
 
-# code-generator does work with go.mod but makes assumptions about
-# the project living in `$GOPATH/src`. To work around this and support
-# any location; create a temporary directory, use this as an output
-# base, and copy everything back once generated.
-TEMP_DIR=$(mktemp -d)
-cleanup() {
-    echo ">> Removing ${TEMP_DIR}"
-    rm -rf ${TEMP_DIR}
-}
-trap "cleanup" EXIT SIGINT
+source "${CODEGEN_PKG}/kube_codegen.sh"
 
-echo ">> Temporary output directory ${TEMP_DIR}"
+THIS_PKG="github.com/openfaas/faas-netes"
 
-# Ensure we can execute.
-chmod +x ${CODEGEN_PKG}/generate-groups.sh
-chmod +x ${CODEGEN_PKG}/generate-internal-groups.sh
+kube::codegen::gen_helpers \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
+    "${SCRIPT_ROOT}/pkg/apis"
 
-${CODEGEN_PKG}/generate-groups.sh all \
-    github.com/openfaas/faas-netes/pkg/client github.com/openfaas/faas-netes/pkg/apis \
-    "openfaas:v1 iam:v1" \
-    --output-base "${TEMP_DIR}" \
-    --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
+kube::codegen::gen_client \
+    --with-watch \
+    --output-dir "${SCRIPT_ROOT}/pkg/client" \
+    --output-pkg "${THIS_PKG}/pkg/client" \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
+    "${SCRIPT_ROOT}/pkg/apis"
 
-# Copy everything back.
-cp -r "${TEMP_DIR}/github.com/openfaas/faas-netes/." "${SCRIPT_ROOT}/"
