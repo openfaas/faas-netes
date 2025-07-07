0363b68d01e12784b35447f547d0c4a60ccf5696
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2025-07-07 17:53:21 +0100

    Lint charts with kubeconform as extra measure
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/lint_chart.sh
+++ contrib/lint_chart.sh
@@ -15,7 +15,37 @@ for chart in $ROOT/chart/*; do
 done
 
 if [ $status != 0 ]; then
-  echo -e "Failures:\n$msg"
+  echo -e "Helm lint failures:\n$msg"
+  exit $status
 fi
 
-exit $status
+
+# Run kubeconform on all charts as an extra measure
+
+for chart in $ROOT/chart/*; do
+  name=$(basename $chart)
+  echo -e "\n\nKubeconform $name"
+  # Use ignore-missing-schemas to avoid errors with missing CRD schemas for the openfaas chart only
+
+  echo "Using kubeconform on $name - $chart"
+
+  if [ "$name" == "openfaas" ]; then
+    helm template chart/$name -f ./chart/openfaas/values-pro.yaml | kubeconform --ignore-missing-schemas -summary --verbose --strict
+  else
+    helm template chart/$name | kubeconform -summary --verbose --strict
+  fi
+  
+  if [ $? != 0 ]; then
+    status=1
+    msg="$msg\n$name"
+  fi
+done
+
+if [ $status != 0 ]; then
+  echo -e "Kubeconform failures:\n$msg"
+  exit $status
+fi
+
+echo -e "\n\nAll charts passed linting and kubeconform validation"
+exit 0
+
